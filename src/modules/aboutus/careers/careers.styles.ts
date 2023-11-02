80116a9db6dcc857be935057082ab9f82a909ae8
topdev0104 <simonweingand73@gmail.com>
2023-11-02 12:02:26 -0400

    feat: carrer section

--- src/modules/aboutus/careers/careers.styles.ts
+++ src/modules/aboutus/careers/careers.styles.ts
@@ -4,4 +4,62 @@ export const CareersWrapper = styled.div`
     max-width: 1240px;
     width: 95%;
     margin: auto;
+    background: var(--bg-grey, #F1F6FA);
+    display: flex;
+    border-radius: 24px;
+    h1{
+        color: #2D6CCB;
+        font-size: 14px;
+        font-style: normal;
+        font-weight: 700;
+        line-height: normal;
+        letter-spacing: 1.4px;
+        text-transform: uppercase;
+    }
+    h2{
+        color: var(--site-heading, #1E2123);
+        font-size: 36px;
+        font-style: normal;
+        font-weight: 700;
+        line-height: normal;
+        margin-top: 24px;
+    }
+     h3{
+       color: var(--text-grey-light, #555);
+        font-size: 16px;
+        font-style: normal;
+        font-weight: 400;
+        line-height: 22px; /* 137.5% */
+        margin: 16px 0 24px 0;
+    }
+    div:first-child{
+        padding: 69px 58px;
+        width: 53%;
+        div{
+            width: fit-content;
+        }
+    }
+    img{
+        width: 47%;
+        border-top-right-radius: 24px;
+         border-bottom-right-radius: 24px;
+         object-fit: cover;
+    }
+    @media screen and (max-width:768px) {
+        flex-direction: column;
+        align-items: center;
+        div{
+            width: 100% !important;
+        }
+        img{
+            width: 90% !important;
+            border-radius: 24px;
+            margin-bottom: 20px;
+        }
+    }
+    @media screen and (max-width:425px) {
+        div:first-child{
+            padding: 40px 20px 20px 20px;
+        }
+    }
 `;
--- src/modules/aboutus/careers/index.tsx
+++ src/modules/aboutus/careers/index.tsx
@@ -1,6 +1,25 @@
 import React from "react";
 import * as Styled from "./careers.styles";
-
+import * as Comp from "@/components";
+import { BsArrowRight } from "react-icons/bs";
 export const Careers: React.FC = () => {
-  return <Styled.CareersWrapper>Investers</Styled.CareersWrapper>;
+  return (
+    <Styled.CareersWrapper>
+      <div>
+        <h1>careers</h1>
+        <h2>Join the Team</h2>
+        <h3>
+          As per the Financial Times, Classe365 is among the rapidly expanding
+          companies in Asia Pacific, and we have even more ambitious plans for
+          the future. Explore our current job openings and send in your resume.
+          We eagerly await your response!
+        </h3>
+        <Comp.Button size="sm">
+          <span>See Open Positions</span>
+          <BsArrowRight />
+        </Comp.Button>
+      </div>
+      <img src={"./assets/images/aboutus/join.png"} alt="carrer" />
+    </Styled.CareersWrapper>
+  );
 };
